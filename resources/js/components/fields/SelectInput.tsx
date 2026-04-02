06b68ae83b401427a0873bcf0f911a85477ff7a6
Scott Johnson <lorem.t.ipsum@gmail.com>
2026-04-02 13:13:15 -0400

    added select component

---
+++ resources/js/components/fields/SelectInput.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+type SelectProps = Omit<React.SelectHTMLAttributes<HTMLSelectElement>, 'id'> & {
+    id: string;
+    label?: string;
+    error?: string;
+    help?: string;
+    labelClass?: string;
+    helpClass?: string;
+    errorClass?: string;
+    options: { value: string; label: string }[];
+};
+
+export default function Select({
+    label,
+    error,
+    help,
+    labelClass,
+    helpClass,
+    errorClass,
+    options,
+    ...props
+}: SelectProps) {
+    return (
+        <>
+            {label && (
+                <label htmlFor={props.id} className={labelClass}>
+                    {label}
+                </label>
+            )}
+
+            <select
+                {...props}
+                aria-invalid={!!error}
+                aria-describedby={[
+                    help ? `help-${props.id}` : null,
+                    error ? `error-${props.id}` : null,
+                ].filter(Boolean).join(' ') || undefined}
+                className={props.className}
+            >
+                {options.map(option => (
+                    <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
+            </select>
+
+            {help && (
+                <div id={`help-${props.id}`} className={helpClass}>
+                    {help}
+                </div>
+            )}
+
+            {error && (
+                <div role="alert" id={`error-${props.id}`} className={errorClass}>
+                    {error}
+                </div>
+            )}
+        </>
+    );
+}
